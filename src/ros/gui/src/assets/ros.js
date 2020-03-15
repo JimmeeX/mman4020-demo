44d0581e9317c3cf33f865958ca3ab95685613b0
James Lin <james.lin1997@hotmail.com>
2020-03-15 03:14:06 -0700

    add skeleton for web GUI; update launch file

---
+++ src/ros/gui/src/assets/ros.js
@@ -0,0 +1,25 @@
+var ros = new ROSLIB.Ros({
+  url: 'ws://localhost:9090'
+});
+
+ros.on('connection', function() {
+  document.getElementById('status').innerHTML = 'Connected';
+});
+
+ros.on('error', function(error) {
+  document.getElementById('status').innerHTML = 'Error';
+});
+
+ros.on('close', function() {
+  document.getElementById('status').innerHTML = 'Closed';
+});
+
+var txt_listener = new ROSLIB.Topic({
+  ros: ros,
+  name: '/txt_msg',
+  messageType: 'std_msgs/String'
+});
+
+txt_listener.subscribe(function(m) {
+  document.getElementById('msg').innerHTML = m.data;
+});
