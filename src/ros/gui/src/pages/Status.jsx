c6d98519c7bc6a2f6500e870bdd7b84c091a4805
James Lin <james.lin1997@hotmail.com>
2020-03-16 06:29:04 -0700

    implement table; status UI

--- src/ros/gui/src/pages/Status.jsx
+++ src/ros/gui/src/pages/Status.jsx
@@ -1,21 +1,81 @@
 import React from 'react';
 
 const Status = () => {
+  const status = [
+    {
+      id: 1,
+      name: 'Water Sampler',
+      type: 'system',
+      connection: true,
+      state: true
+    },
+    {
+      id: 2,
+      name: 'Barometric Sensor',
+      type: 'sensor',
+      connection: false,
+      state: false
+    },
+    {
+      id: 3,
+      name: 'Flow Rate Sensor',
+      type: 'sensor',
+      connection: true,
+      state: true
+    },
+    { id: 4, name: 'Pump', type: 'pump', connection: null, state: true },
+    { id: 5, name: 'Valve 1', type: 'valve', connection: null, state: true },
+    { id: 6, name: 'Valve 2', type: 'valve', connection: null, state: true },
+    { id: 7, name: 'Valve 3', type: 'valve', connection: null, state: false },
+    { id: 8, name: 'Valve 4', type: 'valve', connection: null, state: true },
+    { id: 9, name: 'Valve 5', type: 'valve', connection: null, state: false },
+    { id: 10, name: 'Valve 6', type: 'valve', connection: null, state: false },
+    { id: 11, name: 'Valve 7', type: 'valve', connection: null, state: false }
+  ];
+
+  const connectionToHTML = connection => {
+    switch (connection) {
+      case true:
+        return <p style={{ color: 'green' }}>CONNECTED</p>;
+      case false:
+        return <p style={{ color: 'red' }}>NOT CONNECTED</p>;
+      default:
+        return <p>N/A</p>;
+    }
+  };
+
+  const stateToHTML = (state, type) => {
+    switch (type) {
+      case 'system':
+        return state ? 'ON' : 'OFF';
+      case 'sensor':
+        return state ? 'ON' : 'OFF';
+      case 'pump':
+        return state ? 'ON' : 'OFF';
+      case 'valve':
+        return state ? 'OPEN' : 'CLOSE';
+      default:
+        return 'N/A';
+    }
+  };
+
   return (
-    <div id='status'>
+    <div id='status' className='container'>
       <h2>Status</h2>
-      {/* <table>
+      <table>
         <tr>
           <th>Name</th>
           <th>Connection</th>
           <th>State</th>
         </tr>
-        <tr>
-          <td>System</td>
-          <td>a</td>
-          <td>b</td>
-        </tr>
-      </table> */}
+        {status.map(item => (
+          <tr>
+            <td>{item.name}</td>
+            <td>{connectionToHTML(item.connection)}</td>
+            <td>{stateToHTML(item.state, item.type)}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
