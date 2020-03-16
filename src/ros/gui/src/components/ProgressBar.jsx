9bdc4580e97e6ec92fc423e24e88d9ad071cd8af
James Lin <james.lin1997@hotmail.com>
2020-03-16 05:54:09 -0700

    add progressbar; implement data component UI

--- src/ros/gui/src/components/ProgressBar.jsx
+++ src/ros/gui/src/components/ProgressBar.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+const maxWidth = 200;
+
 const ProgressBar = props => {
-  return <div className='progress-bar'></div>;
+  const { id, name, current, max, units } = props;
+
+  const currWidth = (current / max) * maxWidth;
+
+  return (
+    <div id={id} className='progress-bar'>
+      <p className='progress-bar__name'>{name}</p>
+      <div className='progress-bar__rect' style={{ width: `${maxWidth}px` }}>
+        <div
+          className='progress-bar__rect-max'
+          style={{ width: `${maxWidth}px` }}
+        />
+        <div
+          className='progress-bar__rect-current'
+          style={{ width: `${currWidth}px` }}
+        />
+      </div>
+      <p className='progress-bar__text'>
+        {current}/{max} {units}
+      </p>
+    </div>
+  );
 };
 
 export default ProgressBar;
