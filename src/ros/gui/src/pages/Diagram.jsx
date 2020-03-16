78a43f47e7181f8ca512790af3443b672e0174ef
James Lin <james.lin1997@hotmail.com>
2020-03-16 06:29:04 -0700

    implement table; status UI

--- src/ros/gui/src/pages/Diagram.jsx
+++ src/ros/gui/src/pages/Diagram.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Diagram = () => {
   return (
-    <div id='diagram'>
+    <div id='diagram' className='container'>
       <h1>Water Sampler Control</h1>
     </div>
   );
