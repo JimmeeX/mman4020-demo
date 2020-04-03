de1028d5821e9f0b1c6f35c23a19e0b1c397e39a
ryotokuro <ryotokuro@gmail.com>
2020-04-03 11:45:22 +1100

    added functions for each of the components, yet to test individually

--- src/arduino/pump.ino
+++ src/arduino/pump.ino
@@ -1,7 +1,23 @@
 // Functions to deal with Peristaltic Pump
 
-//class Pump {
-//  public:
-//    void turnOn();
-//    void turnOff();
-//}
+class Pump {
+  private:
+    const int motor;
+    
+  public:
+    int getMotor();
+    void turnOn();
+    void turnOff();
+}
+
+int getMotor() {
+  return motor;
+}
+
+void turnOn() {
+  digitalWrite(getMotor(), HIGH);
+}
+
+void turnOff() {
+  digitalWrite(getMotor(), LOW);
+}
