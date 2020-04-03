e8d38e5ca090ba4c587c209fcdf97882f17951a6
James Lin <james.lin1997@hotmail.com>
2020-04-03 09:16:17 -0700

    implement draft purging function

--- src/ros/sampler/src/main.py
+++ src/ros/sampler/src/main.py
@@ -7,6 +7,7 @@ from actionlib import SimpleActionServer
 from std_msgs.msg import Bool, Float32
 from sampler.msg import (
     PurgeAction,
+    PurgeFeedback,
     PurgeResult,
     SampleAction,
     SampleFeedback,
@@ -21,6 +22,10 @@ from sampler.msg import (
 
 from Sampler import Sampler
 
+import time
+
+SLEEP_RATE = 3 # Hz (ie, 3 times/second)
+
 PURGE_DURATION = 30 # Seconds
 NUM_JARS = 6
 NUM_VALVES = 7
@@ -30,7 +35,7 @@ class Main():
     def __init__(self):
         rospy.loginfo("Starting up Main")
 
-        self.rate = rospy.Rate(3)
+        self.rate = rospy.Rate(SLEEP_RATE)
 
         """Initialise Water Sampler Class"""
         self.sampler = Sampler(JAR_CAPACITIES)
@@ -46,7 +51,16 @@ class Main():
         self.valve7_pub = rospy.Publisher("/arduino/valve7", Bool, queue_size=1) # Purge
         self.pump_pub = rospy.Publisher("/arduino/pump", Bool, queue_size=1)
 
+        # Jar Volumes
+        self.jar1_pub = rospy.Publisher("/volume/jar1", Float32, queue_size=1)
+        self.jar2_pub = rospy.Publisher("/volume/jar2", Float32, queue_size=1)
+        self.jar3_pub = rospy.Publisher("/volume/jar3", Float32, queue_size=1)
+        self.jar4_pub = rospy.Publisher("/volume/jar4", Float32, queue_size=1)
+        self.jar5_pub = rospy.Publisher("/volume/jar5", Float32, queue_size=1)
+        self.jar6_pub = rospy.Publisher("/volume/jar6", Float32, queue_size=1)
+
         """Initialise Subscribers"""
+        # Components
         self.valve1_sub = rospy.Subscriber("/arduino/valve1", Bool, self.handle_valve, (1), queue_size=1) # Jar 1
         self.valve2_sub = rospy.Subscriber("/arduino/valve2", Bool, self.handle_valve, (2), queue_size=1) # Jar 2
         self.valve3_sub = rospy.Subscriber("/arduino/valve3", Bool, self.handle_valve, (3), queue_size=1) # Jar 3
@@ -56,6 +70,7 @@ class Main():
         self.valve7_sub = rospy.Subscriber("/arduino/valve7", Bool, self.handle_valve, (7), queue_size=1) # Purge
         self.pump_sub = rospy.Subscriber("/arduino/pump", Bool, self.handle_pump, queue_size=1)
 
+        # Sensors
         self.flow_sub = rospy.Subscriber("/arduino/flow", Float32, self.handle_flow, queue_size=1) # Flow Rate Sensor
         self.temp_sub = rospy.Subscriber("/arduino/temp", Float32, self.handle_temp, queue_size=1) # Temperature
         self.depth_sub = rospy.Subscriber("/arduino/depth", Float32, self.handle_depth, queue_size=1) # Water Depth
@@ -81,25 +96,22 @@ class Main():
     """CLASS SERVER-SIDE FUNCTIONS"""
     def exec_sample(self, goal):
         print("Execute Sample")
-        print(goal.jars)
-        pass
-        # data = message.data
-        # if message.data == True:
-        #     # TODO:
-        #     """
-        #     1. START PUMPING PROCESS
-        #     2. Read Flow Rate
-        #     """
-        #     print("PUMPING")
-        #     pass
-        # else:
-        #     # TODO: STOP PUMPING
-        #     print("STOPPING")
-        #     pass
+        print(goal.jars) # List of Boolean of Size 6
+        """
+        # TODO
+        1. Check if system is idle
+        2. Open Specified Jar Valves. Ensure rest of valves are closed.
+        3. Run the Pump
+        4. Estimate Jar Capacity (dt * flow rate = volume). You might need to consider stuff like
+            - Location of Jar in Water Block (ie, some jars are filled before others?)
+            - Accuracy of flow rate can be compared with the pump specs && the flow sensor
+        5. When a jar is 'filled', close the valve
+        6. When all jars are filled, stop the process
+        """
 
 
     def exec_purge(self, goal):
-        # TODO
+        # TODO HANDLE STOP REQUEST
         print("Execute Purge")
         """
         1. Check if System is idle
@@ -107,7 +119,53 @@ class Main():
         3. Open purge valve
         4. Turn on Pump
         """
-        pass
+        if self.sampler.state['pump']:
+            # Pump is already running: not idle
+            result = PurgeResult(
+                success=False,
+                message='System is not idle. Please wait for the pump to stop.'
+            )
+            self.action_servers['purge'].set_aborted(result)
+        else:
+            # Close Jar Valves
+            self.set_jar_valves(False)
+
+            # Open Purge Valve
+            self.valve7_pub.publish(True)
+            self.rate.sleep()
+
+            # Turn on Pump
+            self.pump_pub.publish(True)
+            self.rate.sleep()
+
+            # Wait 30 Seconds
+            feedback = PurgeFeedback()
+            counter = 0
+            while counter < PURGE_DURATION * SLEEP_RATE:
+                counter += 1
+                feedback.eta = PURGE_DURATION - (counter // SLEEP_RATE)
+                self.action_servers['purge'].publish_feedback(feedback)
+                self.rate.sleep()
+
+            # Turn off pump
+            self.pump_pub.publish(False)
+            self.rate.sleep()
+
+            # Turn off Purge Valve
+            self.valve7_pub.publish(False)
+            self.rate.sleep()
+
+            # Update Sampler State
+            self.sampler.setPurged(True)
+
+            # Return Message
+            result = PurgeResult(
+                success=True,
+                message='Purging process completed successfully'
+            )
+            self.action_servers['purge'].set_succeeded(result)
+
+
 
 
     def exec_stop(self, goal):
@@ -150,11 +208,7 @@ class Main():
         self.action_servers['pump'].set_succeeded(result)
 
 
-    """Note that this will be run separately for each valve"""
     def exec_set_valve(self, goal):
-        """
-        Forward Message to Arduino
-        """
         print("Received Message to Set Valve " + str(goal.id) + " to " + str(goal.state))
 
         # Change Valve State
@@ -199,6 +253,7 @@ class Main():
 
     """CLASS UTILITY FUNCTIONS"""
     def set_jar_valves(self, value):
+        # Sleep (3hz) maybe so electronics doesn't do everything at once?
         self.valve1_pub.publish(value)
         self.rate.sleep()
 
